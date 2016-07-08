431319a3c9665afa8f5b485be57b25835ad4cc90
Jason Sutter <jasonsutter87@gmail.com>
2016-07-07 20:31:55 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-10.times do
-  User.create(first_name: Faker::Name.first_name,  username: Faker::Superhero.name, email: Faker::Internet.email, password: '123')  
+30.times do
+  User.create(first_name: Faker::Name.first_name,  username: Faker::Superhero.name, email: Faker::Internet.email, password: '123') 
 end
 
-
-50.times do
-	Wish.create(title: Faker::Lorem.word, content: Faker::Lorem.paragraph)
+10.times do
+	Wish.create(user_id: rand(1..10), title: Faker::Lorem.word, content: Faker::Lorem.paragraph, price: 1)
 end
