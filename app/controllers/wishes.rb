04fdfa33bd1853f4adfe612ba503380ba426add6
Jason Sutter <jasonsutter87@gmail.com>
2016-07-09 10:20:43 -0700

    updated controller to add more routes

--- app/controllers/wishes.rb
+++ app/controllers/wishes.rb
@@ -1,4 +1,38 @@
+#index of all wishes
+get '/wishes' do
+	erb :'wishes/index'
+end
+
+# returns a form to create a new wish
+get 'wishes/new' do
+	erb :'wishes/new' 
+end
+
+
+# Create a new wish
+post '/wishes' do
+	redirect '/' 
+end
+
+#display a wish by id
 get '/wishes/:id' do
 	p @wish = Wish.find(params[:id])
 	erb :'wishes/show' 
 end
+
+#returns a form to edit a wish
+get '/wishes/:id/edit' do
+	erb :'wishes/edit'
+end
+
+#edit a wish by an id
+put '/wishes/:id' do
+	redirect '/' 
+end
+
+#delete a wish by an id
+delete '/wishes/:id' do
+ 	redirect '/' 
+end
+
+
