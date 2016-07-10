fec91f62fc41ec5762a7215aefa7cfb9ebc46b2c
Jason Sutter <jasonsutter87@gmail.com>
2016-07-09 23:38:46 -0700

    user can now edit and delete posts

--- app/controllers/wishes.rb
+++ app/controllers/wishes.rb
@@ -1,13 +1,12 @@
 #index of all wishes
 get '/wishes' do
-	erb :'/wishes/index'
+	redirect "/"
 end
 
 # returns a form to create a new wish
 get '/wishes/new' do
 	erb :'wishes/new' 
 end
-
 
 # Create a new wish
 post '/wishes' do
@@ -31,17 +30,22 @@ end
 
 #returns a form to edit a wish
 get '/wishes/:id/edit' do
+  @wish = Wish.find(params[:id])
 	erb :'wishes/edit'
 end
 
 #edit a wish by an id
 put '/wishes/:id' do
-	redirect '/' 
+  @wish = Wish.find(params[:id])
+  @wish.update(params[:wish])
+	redirect '/users/show'  
 end
 
 #delete a wish by an id
 delete '/wishes/:id' do
- 	redirect '/' 
+  @wish = Wish.find(params[:id])
+  @wish.destroy
+ 	redirect '/users/show' 
 end
 
 
