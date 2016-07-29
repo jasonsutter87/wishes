42f262c81c68be6c716b22d83cf47e9168a38525
Jason Sutter <jasonsutter87@gmail.com>
2016-07-29 08:11:45 -0700

    cleaning up the controller

--- app/controllers/wishes.rb
+++ app/controllers/wishes.rb
@@ -1,15 +1,7 @@
 put '/wishes/:id/buy' do
   @wish = Wish.find(params[:id])
   @user = User.find(@wish.user_id)
-  if @wish.user_id != current_user.id  && @wish.price <= current_user.wallet
-     buyers_wallet = current_user.wallet - @wish.price
-     sellers_wallet = @user.wallet + @wish.price
-     new_price = @wish.price + 1
-     @user.update(wallet: sellers_wallet)
-     @current_user.update(wallet: buyers_wallet)
-     @wish.update(user_id: current_user.id ,price: new_price)
-
-  end
+  buy_wish
   redirect '/'  
 end
 
--- app/helpers/wish_helpers.rb
+++ app/helpers/wish_helpers.rb
@@ -1,5 +1,12 @@
 helpers do
   def buy_wish
-    
+	if @wish.user_id != current_user.id  && @wish.price <= current_user.wallet
+     	buyers_wallet = current_user.wallet - @wish.price
+     	sellers_wallet = @user.wallet + @wish.price
+     	new_price = @wish.price + 1
+     	@user.update(wallet: sellers_wallet)
+    	@current_user.update(wallet: buyers_wallet)
+     	@wish.update(user_id: current_user.id ,price: new_price)
+  	end 
   end
 end
